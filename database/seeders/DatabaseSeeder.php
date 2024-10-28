4cd56e351b6b59996f4e165aee782a40ac30db86
Mohammed Maroof <19404@TQ.INV>
2024-10-28 11:52:27 +0400

    inital commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'a@a.com',
+            'password' => '$2y$12$kt6DH3V.hOsZxbIJLNZFEOIaUPB4dSyJL1hepO8GSmE1UgMXvtp3q',
+        ]);
+
+        User::factory()->create([
+            'name' => 'user1',
+            'email' => 'user1@a.com',
+            'password' => '$2y$12$kt6DH3V.hOsZxbIJLNZFEOIaUPB4dSyJL1hepO8GSmE1UgMXvtp3q',
+        ]);
+    }
+}
